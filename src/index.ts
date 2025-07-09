e8cec4ff125dfe88859b4bc19f386386e2e43bed
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-07-09 14:32:37 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get("/", (_req, res) => {
   res.send("Hello Hemal Parmar from TypeScript + Node on EC2! ");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
