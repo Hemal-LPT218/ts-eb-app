3c6aa8df4bc564be857182f438aa942360f6cb25
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-07-29 17:44:20 +0530

    update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,8 @@
 cd /home/ec2-user/ts-eb-app
 
 docker stop docker-node-app || true
-docker rm docker-node-app || true
+
+docker rmi 730335183789.dkr.ecr.ap-south-1.amazonaws.com/demo-repository/docker-node-app:latest || true
 
 echo "Running Docker container..."
 docker run -p 3000:3000 --rm -d --name docker-node-app 730335183789.dkr.ecr.ap-south-1.amazonaws.com/demo-repository/docker-node-app:latest
