be6f7836cf5328b29cf46a7993a961eca51d0d08
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-07-29 16:45:59 +0530

    update Start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/ts-eb-app
-docker run -p 3000:3000  730335183789.dkr.ecr.ap-south-1.amazonaws.com/demo-repository/docker-node-app:latest
+echo "Running Docker container..."
+docker run -p 3000:3000 --rm -d 730335183789.dkr.ecr.ap-south-1.amazonaws.com/demo-repository/docker-node-app:latest | tee -a /tmp/deploy_start.log
